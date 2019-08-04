29570348ad9814199a3d197221f10219e232684b
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-08-04 11:58:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ var currentLine = function (katzDeliLine) {
   if(katzDeliLine.length===0) {
     return "The line is currently empty.";
 }
-var CustomerandNumber =   
+    var CustomerandNumber =    
 }
