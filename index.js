21ff9494f53130203da7ea51dc094d79ce4090d1
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-08-04 12:05:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ function nowServing (katzDeliLine) {
 }
 
 var currentLine = function (katzDeliLine) {
-  if(katzDeliLine.length===0) {
-    return "The line is currently empty.";
+  if (katzDeliLine.length===0) {
+  return "The line is currently empty.";
 }
-    var customerAndNumber = [];
-    for (var i=0; i<katzDeliLine.length; i++) {
-      return customerAndNumber.push(i+1 + ". " + katzDeliLine[i])
+   var customerAndNumber = [];
+   for (var i=0; i<katzDeliLine.length; i++) {
+   customerAndNumber.push(i+1 + ". " + katzDeliLine[i])
 }
-return "The line is currently: " + customerAndNumber.join(",");
+  return "The line is currently: " + customerAndNumber.join(", ");
 }
